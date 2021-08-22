65666c683c6c102c31c410fa8e01ff94de36b9d7
Godnon Dsilva <50076340+godnondsilva@users.noreply.github.com>
2021-08-22 15:13:22 +0530

    Update About.java

--- HotelBookingSystem/src/main/java/com/hotelbooking/frames/About.java
+++ HotelBookingSystem/src/main/java/com/hotelbooking/frames/About.java
@@ -19,30 +19,9 @@ public class About {
         JTextArea textArea = new JTextArea("Hotel Booking System is an application which helps \nit's users get the best deals on their preferred \nhotels. \n\nDevelopment: \nBuilt With Java Swing"); 
         textArea.setEditable(false);
         JScrollPane scrollableTextArea = new JScrollPane(textArea);  
-        scrollableTextArea.setBounds(30, 50, 320, 110);
+        scrollableTextArea.setBounds(30, 50, 320, 210);
         scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
         aboutFrame.add(scrollableTextArea);  
-        
-        // Bottom section
-        JLabel aboutDevelopersText = new JLabel("Developers");
-        aboutDevelopersText.setBounds(150, 180, 150, 30);
-        aboutFrame.add(aboutDevelopersText);
-        
-        JLabel aboutGodnonText = new JLabel("Godnon Praver Dsilva - 4SO19CS056");
-        aboutGodnonText.setBounds(80, 210, 250, 30);
-        aboutFrame.add(aboutGodnonText);
-        
-        JLabel aboutErrolText = new JLabel("Errol Ken Pereira - 4SO19CS050");
-        aboutErrolText.setBounds(80, 230, 250, 30);
-        aboutFrame.add(aboutErrolText);
-        
-        JLabel aboutjasonText = new JLabel("Jason Neil Pinto - 4SO19CS063");
-        aboutjasonText.setBounds(80, 250, 250, 30);
-        aboutFrame.add(aboutjasonText);
-        
-        JLabel aboutGlenvinText = new JLabel("Glenvin Anil Rosario - 4SO19CS055");
-        aboutGlenvinText.setBounds(80, 270, 250, 30);
-        aboutFrame.add(aboutGlenvinText);
         
         JButton goBackBtn = new JButton("Back");
         goBackBtn.setBounds(140,310,100,30);
